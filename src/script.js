797bff2dbd6590f3d8ae11f924805d17de9d4e17
artem.andreev <artem.andreev>
2025-03-15 08:44:32 +0100

    add address link and analytics  for this

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
   document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".link-button").forEach(function (button) {
+    document.querySelectorAll(".link-button,.bottom_address ").forEach(function (button) {
       button.addEventListener("click", function () {
         gtag("event", "link_click", {
           "position": this.getAttribute("data-position"),
